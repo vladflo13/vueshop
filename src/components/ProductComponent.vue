2a5d7405a695e1138abbaa7c908cffa3835035bf
vlad <vl01fl@yahoo.com>
2023-11-08 16:18:00 +0200

    changing outline of productcomponent

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -14,20 +14,28 @@
                 <thead>
                     <tr>
                         <th>Product</th>
-                        <th>Name</th>
+                        <th class="table-name">Name</th>
                         <th>Price(1)</th>
-                        <th>Number</th>
+                        <th class="table-numberProducts">Quantity</th>
                         <th>Total Price</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="product in order" class="order-list" :key="product.id">
-                        <td class="table-product-image" :style="'background-image:url('+ product.src +')'" ></td>
-                        <td>{{ product.title }}</td>
-                        <td>{{ product.price + ' €'}}</td>
-                        <td>{{ product.numberProducts }}</td>
-                        <td>{{ product.price? (product.price * product.numberProducts).toFixed(2) + ' €': null}}</td>
-                        <button class="nutrition-button" @click="CheckNutrition(product.id)"> Check Nutrition</button>
+                    <tr v-for="product in order" :key="product.id" >
+                        <div class="order-list" v-if="product.numberProducts > 0 && !isShrunk">
+                            <td class="table-product-image" :style="'background-image:url('+ product.src +')'" ></td>
+                            <td class="table-name">{{ product.title }}</td>
+                            <td>{{ product.price + ' €'}}</td>
+                            <td class="table-numberProducts">
+                                <button class="product-modify-button" @click="IncreaseProductNumber(product.id)">+</button>
+                                <input class="number-product-input" :id="'input-'+ product.id" :value="product.numberProducts">
+                                <button class="product-modify-button" @click="DecreaseProductNumber(product.id)">-</button>
+                            </td>
+                            <td>{{ product.price? (product.price * product.numberProducts).toFixed(2) + ' €': null}}</td>
+                            <td class="last-item">                        
+                                <button class="nutrition-button" @click="CheckNutrition(product.id)">Check Nutrition</button>
+                            </td>
+                        </div>
                     </tr>
                 </tbody>
             </div>
@@ -37,8 +45,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { imgObject, orderProduct, productDTO } from '@/interfaces';
-import OrderItem from './OrderItem.vue';
+import { orderProduct } from '@/interfaces';
 
 export default defineComponent({
     data() {
@@ -55,6 +62,16 @@ export default defineComponent({
     mounted(){
         const shrink = this.$refs.shrink as HTMLButtonElement;
         shrink.focus();
+        const inputs = document.querySelectorAll('input');
+        inputs.forEach(input =>{
+            input.addEventListener('change', this.HandleInputChange)
+        })
+    },
+    beforeUnmount(){
+        const inputs = document.querySelectorAll('input');
+        inputs.forEach(input =>{
+            input.removeEventListener('change', this.HandleInputChange)
+        })
     },
     methods:{
         Shrink(){
@@ -62,6 +79,19 @@ export default defineComponent({
         },
         CheckNutrition(id:number){
             console.log(id)
+        },
+        IncreaseProductNumber(id:number){
+            const productNumberInput = document.getElementById('input-'+id) as HTMLInputElement;
+            productNumberInput.value ? productNumberInput.value = (parseInt(productNumberInput.value) + 1).toString(): null;
+        },
+        DecreaseProductNumber(id:number){            
+            const productNumberInput = document.getElementById('input-'+id) as HTMLInputElement;
+            productNumberInput.value ? productNumberInput.value = (parseInt(productNumberInput.value) - 1).toString() : null;
+        },
+        HandleInputChange(event:Event){
+            console.log('change');
+            if(event.target)
+                console.log(event.target);
         }
     },
     components: { }
@@ -72,6 +102,10 @@ export default defineComponent({
 .product-container{
     --column-width:90px;
     --column-height:40px;
+    --shrink-height: 33%;
+    --shrink-width: 20%;
+    --shrink-top: 25%;
+    --shrink-left: -10%;
     position: fixed;
     top: 15%;
     left: 10%;
@@ -90,28 +124,30 @@ export default defineComponent({
     border: 2px solid var(--primary-color);
     transition: all 0.5s ease;
     pointer-events: all;
-
 }
 .shrink{
     animation-name: shrink;
     animation-duration: 0.5s;
     pointer-events: none;
     font-size: 0px;
-    width: 0%;
-    height: 0%;
+    top:var(--shrink-top);
+    left:var(--shrink-left);
+    width: var(--shrink-width);
+    height: var(--shrink-height);
 }
 .top-left-section{
     position: absolute;
     z-index: 4;
-    top:0px;
-    left:0px;
-    width: 20%;
-    height: 33%;
+    top:var(--shrink-top);
+    left:var(--shrink-left);
+    width: var(--shrink-width);
+    height: var(--shrink-height);
     border: 2px solid var(--primary-color); 
     background-color: color-mix(in lch,var(--background-color) 100%, transparent 5%);
     pointer-events: all;
 
 }
+
 .shrink-me{
     width: 100%;
     height: 100%;
@@ -125,11 +161,10 @@ export default defineComponent({
 
 .right-section{
     position: absolute;
-    left: 20%;
+    left: 10%;
     top: 0%;
     width: 80%;
     height: 100%;
-    outline: 2px solid black;
     display: flex;
     flex-direction: column;
     text-align: center;
@@ -137,6 +172,7 @@ export default defineComponent({
 .right-section > h1{
     margin: 5px;
 }
+
 tr{
     display: flex;
     flex-direction: row;
@@ -150,22 +186,56 @@ th{
 td{
     width: var(--column-width);
     height: var(--column-height);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-    text-align: center;
     overflow: hidden;
     background-position: center;
     background-size: cover;
 }
 
+.table-name{
+    width: calc(var(--column-width)*2);
+}
+.table-numberProducts{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 10px;
+}
+.product-modify-button{
+    width: 50px;
+    height: 50px;
+    border: 2px solid var(--primary-color);
+    border-radius: 20%;
+}
+.number-product-input{
+    width: 50px;
+    height: 50px;
+    color: var(--primary-color);
+    background-color: var(--background-color);
+    border: 2px solid var(--primary-color);
+}
+.last-item{
+    flex-grow: 2;
+    display: flex;
+    flex-direction: row-reverse;
+}
 .table-product-image{
     border: 1px solid var(--primary-color);
     margin-bottom: 5px;
 }
 .nutrition-button{
-    text-align: top;
+    width: var(--column-width);
+    height: 100%;
+    border: 2px solid var(--primary-color);
+    margin-right: 10px;
 }
 .order-list{
+    width: 100%;
     margin-left:5px;
+    position: relative;
     display: flex;
     flex-direction: row;
     align-items: center;
